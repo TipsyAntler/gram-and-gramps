33ea3d6c6ca5520847008721f47b56faeb0d3832
TipsyAntler <51962438+TipsyAntler@users.noreply.github.com>
2026-08-10 14:30:05 -0400

    Add direct public GEDCOM data

---
+++ assets/js/tree-data.js
@@ -0,0 +1,118 @@
+window.PUBLIC_GEDCOM = String.raw`0 HEAD
+1 SOUR Tridente Family Oral History
+2 NAME Gram & Gramps Family Tree
+1 GEDC
+2 VERS 5.5.1
+2 FORM LINEAGE-LINKED
+1 CHAR UTF-8
+0 @I182064743652@ INDI
+1 NAME Sergio "Sam" /Tridente/
+1 SEX M
+1 BIRT
+2 DATE 20 MAY 1925
+2 PLAC Hoboken, Hudson, New Jersey, USA
+1 DEAT
+2 DATE 2 JAN 2019
+1 FAMS @F1@
+1 FAMC @F2@
+1 NOTE Oral history connections
+2 CONT Hear Gramps' stories: https://tipsyantler.github.io/gram-and-gramps/stories.html?speaker=Gramps
+0 @I182064743653@ INDI
+1 NAME Marietta /Cappelluti/
+1 SEX F
+1 BIRT
+2 DATE 27 APR 1928
+2 PLAC Hoboken, Hudson, New Jersey, USA
+1 DEAT
+2 DATE 7 JUL 2021
+1 FAMS @F1@
+1 FAMC @F3@
+1 NOTE Oral history connections
+2 CONT Hear Gram's stories: https://tipsyantler.github.io/gram-and-gramps/stories.html?speaker=Gram
+0 @I182064743654@ INDI
+1 NAME Conrad /Tridente/
+1 SEX M
+1 BIRT
+2 DATE 1948
+1 FAMC @F1@
+1 NOTE Oral history connections
+2 CONT Stories featuring Conrad: https://tipsyantler.github.io/gram-and-gramps/stories.html?person=Conrad
+0 @I182064743655@ INDI
+1 NAME Linda /Tridente/
+1 SEX F
+1 FAMC @F1@
+0 @I182064743656@ INDI
+1 NAME Christine /Tridente/
+1 SEX F
+1 FAMC @F1@
+0 @I182064743657@ INDI
+1 NAME Corrado /Tridente/
+2 NICK Papa
+1 SEX M
+1 FAMS @F2@
+1 NOTE Oral history connections
+2 CONT Gramps Enlisting in the Navy: https://tipsyantler.github.io/gram-and-gramps/story.html?id=14
+2 CONT Gramps Childhood — First Jobs & Street Games: https://tipsyantler.github.io/gram-and-gramps/story.html?id=19
+2 CONT Papa Docks & Mafia Run-Ins: https://tipsyantler.github.io/gram-and-gramps/story.html?id=20
+0 @I182064743658@ INDI
+1 NAME Giovanna "Jennie" /Sciancalepore/
+2 NICK Mama
+1 SEX F
+1 FAMS @F2@
+1 NOTE Oral history connections
+2 CONT Gramps Enlisting in the Navy: https://tipsyantler.github.io/gram-and-gramps/story.html?id=14
+2 CONT Grass Skirt Scam & Hula Girl Pictures: https://tipsyantler.github.io/gram-and-gramps/story.html?id=17
+2 CONT Gramps Childhood — First Jobs & Street Games: https://tipsyantler.github.io/gram-and-gramps/story.html?id=19
+2 CONT Papa Docks & Mafia Run-Ins: https://tipsyantler.github.io/gram-and-gramps/story.html?id=20
+0 @I182064743659@ INDI
+1 NAME Sergio /Cappelluti/
+2 NICK Nunpop
+1 SEX M
+1 FAMS @F3@
+1 NOTE Oral history connections
+2 CONT Pulling 3 Teeth: https://tipsyantler.github.io/gram-and-gramps/story.html?id=4
+2 CONT Having Dad: https://tipsyantler.github.io/gram-and-gramps/story.html?id=24
+0 @I182064743660@ INDI
+1 NAME Sabina /Masellis/
+2 NICK Mamom
+1 SEX F
+1 FAMS @F3@
+1 NOTE Oral history connections
+2 CONT Negotiating with Stevens: https://tipsyantler.github.io/gram-and-gramps/story.html?id=8
+2 CONT Having Dad: https://tipsyantler.github.io/gram-and-gramps/story.html?id=24
+0 @I182064743661@ INDI
+1 NAME Elizabeth "Betty" /Cappelluti/
+1 SEX F
+1 FAMC @F3@
+1 NOTE Oral history connections
+2 CONT The Detective Story: https://tipsyantler.github.io/gram-and-gramps/story.html?id=1
+0 @I182064743662@ INDI
+1 NAME Morris /Tridente/
+1 SEX M
+1 FAMC @F2@
+1 NOTE Oral history connections
+2 CONT Gramps Intro: https://tipsyantler.github.io/gram-and-gramps/story.html?id=13
+0 @I182064743663@ INDI
+1 NAME Louis /
+1 SEX M
+1 NOTE Oral history connections
+2 CONT The Hat & Navy Uniform Life: https://tipsyantler.github.io/gram-and-gramps/story.html?id=21
+2 CONT Getting Stopped for Being Cute & Gramps' Hat: https://tipsyantler.github.io/gram-and-gramps/story.html?id=25
+0 @F1@ FAM
+1 HUSB @I182064743652@
+1 WIFE @I182064743653@
+1 CHIL @I182064743654@
+1 CHIL @I182064743655@
+1 CHIL @I182064743656@
+0 @F2@ FAM
+1 HUSB @I182064743657@
+1 WIFE @I182064743658@
+1 CHIL @I182064743652@
+1 CHIL @I182064743662@
+0 @F3@ FAM
+1 HUSB @I182064743659@
+1 WIFE @I182064743660@
+1 CHIL @I182064743653@
+1 CHIL @I182064743661@
+0 TRLR
+`;
